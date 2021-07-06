d5d9058a3fc6d5f85e0609022c0df9b3577a1ada
ninjalante <77864271+ninjalante@users.noreply.github.com>
2021-07-06 16:41:34 +0900

    Add files via upload

---
+++ waycooks.js
@@ -0,0 +1,8 @@
+/* make a comments section */
+//input boxes: name (mandatory), email (hidden), journal
+//submit button
+//whatever's put into the input box shows up on the website - but then the input box needs to be cleared
+//regex -- any way to censor swear words?
+//how does one delete comments from the website after they've been submitted?
+
+/* make a poll for favorite recipe */
