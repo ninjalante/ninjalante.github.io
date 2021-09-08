1606f1f01c2d3f5cc96ec9fdaef71c8aaa5b9a0c
Lucy from Delaware <77864271+ninjalante@users.noreply.github.com>
2021-09-09 08:21:08 +0900

    improved CSS for transition to second page, clickthebutton script, additional index.html, first two booleans

---
+++ Xmas_movie_chooser/moviechooser.js
@@ -0,0 +1,4 @@
+function clickTheButton() {
+    document.querySelector("#landingPage").style.opacity = "0%";
+   document.querySelector("#movieChooser").style.opacity = "100%";
+   };
--- Xmas_movie_chooser/script.js
+++ Xmas_movie_chooser/script.js
@@ -4,7 +4,6 @@ let XmasMovies = [
         myScore: 100,
         summary: "A trio of overwhelmed mom friends decide to take Christmas easy this year...until their needy moms come to town.",
         comments: "If you're looking to unwind and have a laugh on your own while you're wrapping presents or baking cookies, this movie is for you.",
-        good: true,
         inColor: true,
         romance: true,
         musical: false,
@@ -31,7 +30,6 @@ let XmasMovies = [
         myScore: 100,
         summary: "A little girl discovers that Santa Claus is real after he works as a Santa Claus at her mother's department store.",
         comments: "A Christmas classic that will make anyone wish Santa were real.",
-        good: true,
         inColor: false,
         romance: true,
         musical: false,
@@ -58,7 +56,6 @@ let XmasMovies = [
         myScore: 95,
         summary: "A vacationing NYPD officer flies to L.A. to reconcile with his estranged wife, only to find her workplace Christmas party overrun by terrorists.",
         comments: "It's a perfect movie but it doesn't exactly get me into the Christmas spirit.",
-        good: true,
         inColor: true,
         romance: false,
         musical: false,
@@ -85,7 +82,6 @@ let XmasMovies = [
         myScore: 56,
         summary: "A journalist breaks into a foreign royal family's home and poses as a new employee so she can get the scoop on the heir's plans to take the throne...or not.",
         comments: "It's not good in the bad sort of way, or bad in the good sort of way. It's just bad in the bad sort of way.",
-        good: false,
         inColor: true,
         romance: true,
         musical: false,
@@ -109,7 +105,10 @@ let XmasMovies = [
     }
 ]
 
-    /*  good: 
+    /*  title:
+        myScore:
+        summary:
+        comments: 
         inColor: 
         romance: 
         musical: 
