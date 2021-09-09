8307bde7346baf7a3f13090cee02ca4804f00ab9
Lucy from Delaware <77864271+ninjalante@users.noreply.github.com>
2021-09-09 09:31:40 +0900

    just a couple little changes before the big code push

--- Xmas_movie_chooser/moviechooser.js
+++ Xmas_movie_chooser/moviechooser.js
@@ -1,4 +1,6 @@
 function clickTheButton() {
     document.querySelector("#landingPage").style.opacity = "0%";
-   document.querySelector("#movieChooser").style.opacity = "100%";
+    document.querySelector("#movieChooser").style.opacity = "100%";
+    document.querySelector("#movieChooser").style.visibility = "visible";
+    
    };
