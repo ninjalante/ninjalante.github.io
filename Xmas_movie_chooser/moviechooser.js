2a9486baaa040efe43b3325e3586e70b48a6abc0
ninjalante <77864271+ninjalante@users.noreply.github.com>
2021-11-10 16:58:34 +0900

    adding functionality for transitions and titles

--- Xmas_movie_chooser/moviechooser.js
+++ Xmas_movie_chooser/moviechooser.js
@@ -1,3 +1,4 @@
+//click the button on the landing page to open the quiz
 function clickTheButton() { 
     document.querySelector("#landingPage").style.opacity = "0%";
     document.querySelector("#movieChooser").style.opacity = "100%";
@@ -5,6 +6,7 @@ function clickTheButton() {
     
    };
 
+// -------------------- choose a movie with a perfect score only -----------------
 function choosePerfectMovie() {
   let perfectMovies = [];
   for (i = 0; i < XmasMovies.length; i++){
@@ -12,22 +14,25 @@ function choosePerfectMovie() {
      perfectMovies.push(XmasMovies[i].title);
       }
   }
+  document.querySelector("#movieChooser").style.opacity = "0%";
+  document.querySelector("#movieChooser").style.visibility = "hidden";
+  document.querySelector("#movieRec").style.opacity = "100%";
+  document.querySelector("#movieRec").style.visibility = "visible";
+  
   let movieOutput = document.querySelector("#movieRecommendations");
-  console.log(perfectMovies);
+
+  console.log(perfectMovies);//check code in console
   for (let movie of perfectMovies) {
       movieOutput.innerHTML += '<h1>' + movie + '</h1>'
     }
-    document.querySelector("#movieChooser").style.opacity = "0%";
-    document.querySelector("#movieChooser").style.visibility = "hidden";
 };
+//end of perfect movie function 
 
+//---------------- choosing elements ----------------------
+//make an array for the choices
 let booleanChoices = [];
 
-function chooseColor() {
-  booleanChoices.push(document.querySelector("#inColor").value);
-  let snark = document.querySelector("#snark")
-     snark.innerHTML += '<em> Seriously?</em>'
-}
+//make a function for each choice; will push the movie element to the array
 
 function chooseRomance() {
   booleanChoices.push(document.querySelector("#romance").value);
@@ -77,18 +82,6 @@ function chooseFresh() {
   booleanChoices.push(document.querySelector("#freshStart").value);
 }
 
-function choosePrecociousChild() {
-  booleanChoices.push(document.querySelector("#precocious").value);
-}
-
-function chooseBaker() {
-  booleanChoices.push(document.querySelector("#baker").value);
-}
-
-function chooseDeadParent() {
-  booleanChoices.push(document.querySelector("#deadParent").value);
-}
-
 function chooseFamousActor() {
   booleanChoices.push(document.querySelector("#famousActor").value);
 }
@@ -105,7 +98,7 @@ function choosePilf() {
   booleanChoices.push(document.querySelector("#pilf").value);
 }
 
-//final function for button
+//------------- choosing movie function -------------
 
 function chooseMovie() {
   //make sure each element is only listed once. this part is done!
@@ -116,7 +109,7 @@ function chooseMovie() {
             }
         }
      );
-  //find the elements in the XmasMovies array
+  //make arrays for movie scores, titles, etc.
  let partialMatches = [];
  let summaries = [];
  let myScores = [];
@@ -124,7 +117,7 @@ function chooseMovie() {
  //test code in console
  console.log(partialMatches)
  console.log(chosenElementsUniq);
- 
+ //push titles into "partialMatches", same number of times they have been chosen
  for (i = 0; i < XmasMovies.length; i++){
    if (XmasMovies[i].romance == true && chosenElementsUniq.includes("romance")) {
     partialMatches.push(XmasMovies[i].title);
@@ -199,24 +192,7 @@ function chooseMovie() {
      myScores.push(XmasMovies[i].myScore);
      myComments.push(XmasMovies[i].comments);
    }
-   if (XmasMovies[i].precociousChild == true && chosenElementsUniq.includes("precocious")) {
-     partialMatches.push(XmasMovies[i].title);
-     summaries.push(XmasMovies[i].summary);
-     myScores.push(XmasMovies[i].myScore);
-     myComments.push(XmasMovies[i].comments);
-   }
-   if (XmasMovies[i].charmingBaker == true && chosenElementsUniq.includes("baker")) {
-     partialMatches.push(XmasMovies[i].title);
-     summaries.push(XmasMovies[i].summary);
-     myScores.push(XmasMovies[i].myScore);
-     myComments.push(XmasMovies[i].comments);
-   }
-   if (XmasMovies[i].deadParent == true && chosenElementsUniq.includes("deadParent")) {
-     partialMatches.push(XmasMovies[i].title);
-     summaries.push(XmasMovies[i].summary);
-     myScores.push(XmasMovies[i].myScore);
-     myComments.push(XmasMovies[i].comments);
-   }
+  
    if (XmasMovies[i].famousActor == true && chosenElementsUniq.includes("famousActor")) {
      partialMatches.push(XmasMovies[i].title);
      summaries.push(XmasMovies[i].summary);
@@ -241,21 +217,28 @@ function chooseMovie() {
      myScores.push(XmasMovies[i].myScore);
      myComments.push(XmasMovies[i].comments);
    }
-   if (XmasMovies[i].inColor == true && chosenElementsUniq.includes("inColor")) {
-     partialMatches.push(XmasMovies[i].title);
-     summaries.push(XmasMovies[i].summary);
-     myScores.push(XmasMovies[i].myScore);
-     myComments.push(XmasMovies[i].comments);
-   }
  }
   
 //filter movie titles so that only the ones that are mentioned as many times as booleanChoices.length are published
+//This is the tricky part. We need: if partialMatches.count("title") === booleanChoices.length { finalChoices.push("title")}
 let finalChoices = [];
+
+//-------------- get the final recommendations from the partial matches array -------------
+
+
+
   
  //publish movie titles 
- let movieOutput = document.querySelector("#movieRecommendations");
- document.querySelector("#movieChooser").style.opacity = "0%";
- document.querySelector("#movieChooser").style.visibility = "hidden";
+ 
+ 
+  document.querySelector("#movieChooser").style.opacity = "0%";
+  document.querySelector("#movieChooser").style.visibility = "hidden";
+  document.querySelector("#movieRec").style.opacity = "100%";
+  document.querySelector("#movieRec").style.visibility = "visible";
+
+  let movieOutput = document.querySelector("#movieRecommendations");
+
+ //------when you're done this needs to change to finalChoices---------
  if (partialMatches.length == 0) {
   movieOutput.innerHTML += '<h1>There is no movie that matches your search results. Please try again.</h1>'
 } else {
@@ -263,10 +246,8 @@ let finalChoices = [];
   movieOutput.innerHTML += '<h1>' + movie + '</h1>'
   }
  }
+ //--------- END of the movie chooser function: final curly brace below ------------ END OF FUNCTION END OF FUNCTION
 }
    
 //publish some details too?
 //have those pop up when you hover over the title?
-
- 
-//end of the movie chooser function
