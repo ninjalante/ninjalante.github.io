6ec6bcca329a47b9ad49c4d2c76249bba7e5ff5b
Lucy from Delaware <77864271+ninjalante@users.noreply.github.com>
2024-08-03 16:09:06 +0900

    big changes, scroll smooth, content

--- Seoul_Players_2024/script.js
+++ Seoul_Players_2024/script.js
@@ -1,3 +1,20 @@
-function transition() {
+window.onload = function(){
+    let readMore = document.getElementById('read-more');
 
+    readMore.addEventListener('click', function(e){
+     smoothScroll({toElement: document.getElementById('myDiv'), duration: 1300 });
+});
+}
+/*
+let pageTitle = document.getElementById("main-title");
+let pageContent = document.getElementById("main-content");
+function transition(page) {
+    console.log("Prepare to be boarded!");
+    pageTitle.style.visibility = "hidden";
+    pageContent.style.visibility = "hidden";
+    let mainTitle = page.document.getElementById("main-title");
+    let mainContent = page.document.getElementById("main-content");
+    mainTitle.style.visibility = "visible";
+    mainContent.style.visibility = "visible";
 };
+*/
